43a691c1f71fdaf40cdfcd3f87c3df7d13766ecc
oscar-monsalve <oscarmonsalvec5@gmail.com>
2024-07-28 19:18:53 -0500

    update

--- exercises/029_errdefer.zig
+++ exercises/029_errdefer.zig
@@ -17,7 +17,10 @@ const std = @import("std");
 
 var counter: u32 = 0;
 
-const MyErr = error{ GetFail, IncFail };
+const MyErr = error{
+    GetFail,
+    IncFail,
+};
 
 pub fn main() void {
     // We simply quit the entire program if we fail to get a number:
@@ -32,15 +35,18 @@ fn makeNumber() MyErr!u32 {
 
     // Please make the "failed" message print ONLY if the makeNumber()
     // function exits with an error:
-    std.debug.print("failed!\n", .{});
 
-    var num = try getNumber(); // <-- This could fail!
+    {
+        errdefer std.debug.print("failed!\n", .{});
 
-    num = try increaseNumber(num); // <-- This could ALSO fail!
+        var num = try getNumber(); // <-- This could fail!
 
-    std.debug.print("got {}. ", .{num});
+        num = try increaseNumber(num); // <-- This could ALSO fail!
 
-    return num;
+        std.debug.print("got {}. ", .{num});
+
+        return num;
+    }
 }
 
 fn getNumber() MyErr!u32 {
